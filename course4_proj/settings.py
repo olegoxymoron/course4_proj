d911dd0d25f199fb32a158944ec30009663aec96
André P <andrepollux@icloud.com>
2022-11-07 17:38:07 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -51,6 +51,7 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'django_celery_results',
+        'django_celery_beat',
         'movies',
         'gh'
     ]
